4d1b05e16a7e59f73fd09b38a5a17d994404e147
Murat Altun <mraltun@gmail.com>
2022-10-09 12:44:50 +0300

    Added public folder and initial CSS

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3500;
+const path = require("path");
+const PORT = process.env.PORT || 8000;
+
+app.use("/", express.static(path.join(__dirname, "/public")));
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
