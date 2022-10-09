97ad3e4c12cc94657f5f10f735b31012ebbcdf33
Murat Altun <mraltun@gmail.com>
2022-10-09 17:28:10 +0300

    Added dbConn to connect Mongo DB

--- server/server.js
+++ server/server.js
@@ -2,14 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const { logger } = require("./middleware/logger");
+const { logger, logEvents } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 8000;
 
 console.log(process.env.NODE_ENV);
+
+// Connect to DB
+connectDB();
 
 // Logging
 app.use(logger);
@@ -39,4 +44,15 @@ app.all("*", (req, res) => {
 // Error handler mw
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+});
+
+mongoose.connection.on("error", (error) => {
+  console.log(error);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
