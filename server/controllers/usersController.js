1dc9dc9bc85ae7dfc3134c14a32fc67f29e3b829
Murat Altun <mraltun@gmail.com>
2022-10-10 12:18:15 +0300

    userRoutes is done. Some methods in usersController is done

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -6,12 +6,32 @@ const bcrypt = require("bcrypt");
 // @desc Get all users
 // @route GET /users
 // @access Private
-const getAllUsers = asyncHandler(async (req, res) => {});
+const getAllUsers = asyncHandler(async (req, res) => {
+  // Don't return "password" field. Use lean to return JS object instead full Mongo Document.
+  const users = await User.find().select("-password").lean();
+  if (!users) {
+    return res.status(400).json({ message: "No users found" });
+  }
+  res.json(users);
+});
 
 // @desc Create new user
 // @route POST /users
 // @access Private
-const createNewUser = asyncHandler(async (req, res) => {});
+const createNewUser = asyncHandler(async (req, res) => {
+  const { username, password, roles } = req.body;
+
+  // Confirm data
+  if (!username || !password || !Array.isArray(roles) || !roles.length) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  // Check for duplicate. Exec gives better stack traces.
+  const duplicate = await User.findOne({ username }).lean().exec();
+  if (duplicate) {
+    return res.status(409).json({ message: "Duplicate username" });
+  }
+});
 
 // @desc Update a user
 // @route PATCH /users
