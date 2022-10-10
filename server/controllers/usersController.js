aab30e7fb4238b084360c035bc99414ed40fdf7a
Murat Altun <mraltun@gmail.com>
2022-10-10 13:20:30 +0300

    usersController finished

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -26,21 +26,97 @@ const createNewUser = asyncHandler(async (req, res) => {
     return res.status(400).json({ message: "All fields are required" });
   }
 
-  // Check for duplicate. Exec gives better stack traces.
+  // Check for duplicate. Exec returns promise and gives better stack traces.
   const duplicate = await User.findOne({ username }).lean().exec();
   if (duplicate) {
     return res.status(409).json({ message: "Duplicate username" });
+  }
+
+  // Hash the password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const userObject = { username, password: hashedPassword, roles };
+
+  // Create and store new user
+  const user = await User.create(userObject);
+  if (user) {
+    res.status(201).json({ message: `New user ${username} created` });
+  } else {
+    res.status(400).json({ message: "Invalid user data received" });
   }
 });
 
 // @desc Update a user
 // @route PATCH /users
 // @access Private
-const updateUser = asyncHandler(async (req, res) => {});
+const updateUser = asyncHandler(async (req, res) => {
+  const { id, username, roles, active, password } = req.body;
+
+  // Confirm data
+  if (
+    !id ||
+    !username ||
+    typeof active !== "boolean" ||
+    !Array.isArray(roles) ||
+    !roles.length
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  // No "lean" because we need this to be full MongoDB Document that has methods like "save" below
+  const user = await User.findById(id).exec();
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  // Check for duplicate
+  const duplicate = await User.findOne({ username }).lean().exec();
+  // Allow updates to original user
+  if (duplicate && duplicate?._id.toString() !== id) {
+    return res.status(409).json({ message: "Duplicate username" });
+  }
+
+  // Update the user object in the Mongo Document. If we try to set something doesn't already exist in our model it would reject it.
+  user.username = username;
+  user.roles = roles;
+  user.active = active;
+
+  // Update the password only when it is updated
+  if (password) {
+    user.password = await bcrypt.hash(password, 10);
+  }
+
+  const updatedUser = await user.save();
+
+  res.json({ message: `${updatedUser.username} updated` });
+});
 
 // @desc Delete a user
 // @route DELETE /users
 // @access Private
-const deleteUser = asyncHandler(async (req, res) => {});
+const deleteUser = asyncHandler(async (req, res) => {
+  const { id } = req.body;
+
+  if (!id) {
+    return res.status(400).json({ message: "User ID required" });
+  }
+
+  // Find if the user has notes and don't delete the user
+  const notes = await Note.findOne({ user: id }).lean().exec();
+  if (notes?.length) {
+    return res.status(400).json({ message: "User has assigned notes" });
+  }
+
+  const user = await User.findById(id).exec();
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  // Delete the user but hold user info inside "result"
+  const result = await user.deleteOne();
+  const reply = `Username ${result.username} with ID ${result._id} deleted`;
+
+  res.json(reply);
+});
 
 module.exports = { getAllUsers, createNewUser, updateUser, deleteUser };
