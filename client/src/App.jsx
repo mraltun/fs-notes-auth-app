572aa7570a456533a1738bc2a107ddabd93f74d6
Murat Altun <mraltun@gmail.com>
2022-10-10 15:54:45 +0300

    Added NotesList and UsersList along with their routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Public from "./components/Public";
 import Login from "./features/auth/Login";
 import DashLayout from "./components/DashLayout";
 import Welcome from "./features/auth/Welcome";
+import NotesList from "./features/notes/NotesList";
+import UsersList from "./features/users/UsersList";
 
 const App = () => {
   return (
@@ -15,6 +17,14 @@ const App = () => {
 
         <Route path='dash' element={<DashLayout />}>
           <Route index element={<Welcome />} />
+
+          <Route path='notes'>
+            <Route index element={<NotesList />} />
+          </Route>
+
+          <Route path='users'>
+            <Route index element={<UsersList />} />
+          </Route>
         </Route>
       </Route>
     </Routes>
