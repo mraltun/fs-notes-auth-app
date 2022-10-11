1801d40e3d00bb26bb49c6d8ad280dfc4480f8ad
Murat Altun <mraltun@gmail.com>
2022-10-11 12:10:55 +0300

    Added RTK and Query slice and store

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/*' element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
