2fddc2f3d0f4ed987600f6ace40e4bbcdf39114c
Murat Altun <mraltun@gmail.com>
2022-10-11 16:29:12 +0300

    Added rest of the endpoints in notesApiSlice

--- client/src/features/notes/notesApiSlice.js
+++ client/src/features/notes/notesApiSlice.js
@@ -35,11 +35,44 @@ export const notesApiSlice = apiSlice.injectEndpoints({
         } else return [{ type: "note", id: "LIST" }];
       },
     }),
+    addNewNote: builder.mutation({
+      query: (initialNote) => ({
+        url: "/notes",
+        method: "POST",
+        body: {
+          ...initialNote,
+        },
+      }),
+      invalidatesTags: [{ type: "Note", id: "LIST" }],
+    }),
+    updateNote: builder.mutation({
+      query: (initialNote) => ({
+        url: "/notes",
+        method: "PATCH",
+        body: {
+          ...initialNote,
+        },
+      }),
+      invalidatesTags: (result, error, arg) => [{ type: "Note", id: arg.id }],
+    }),
+    deleteNote: builder.mutation({
+      query: ({ id }) => ({
+        url: `/notes`,
+        method: "DELETE",
+        body: { id },
+      }),
+      invalidatesTags: (result, error, arg) => [{ type: "Note", id: arg.id }],
+    }),
   }),
 });
 
 // Automatically created "useXXXXQuery"
-export const { useGetNotesQuery } = notesApiSlice;
+export const {
+  useGetNotesQuery,
+  useAddNewNoteMutation,
+  useUpdateNoteMutation,
+  useDeleteNoteMutation,
+} = notesApiSlice;
 
 // Returns the query result object
 export const selectNotesResult = notesApiSlice.endpoints.getNotes.select();
