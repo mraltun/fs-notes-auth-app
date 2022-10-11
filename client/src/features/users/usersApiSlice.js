9a739fc521667be26273e299484276165268f98b
Murat Altun <mraltun@gmail.com>
2022-10-11 13:49:35 +0300

    Finished usersApiSlice

--- client/src/features/users/usersApiSlice.js
+++ client/src/features/users/usersApiSlice.js
@@ -8,10 +8,13 @@ export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getUsers: builder.query({
       query: () => "/users",
+      // RTK Query always returns a 200, but sets an `isError` property when there is an error.
       validateStatus: (response, result) => {
         return response.status === 200 && !result.isError;
       },
+      // If you query an endpoint, then unmount the component, then mount another component that makes the same request within the given time frame (5 sec), the most recent value will be served from the cache.
       keepUnusedDataFor: 5,
+      // Match id with MongoDB "_id" before query hit the cache
       transformResponse: (responseData) => {
         const loadedUsers = responseData.map((user) => {
           user.id = user._id;
@@ -31,6 +34,7 @@ export const usersApiSlice = apiSlice.injectEndpoints({
   }),
 });
 
+// Automatically created "useXXXXQuery"
 export const { useGetUsersQuery } = usersApiSlice;
 
 // Returns the query result object
