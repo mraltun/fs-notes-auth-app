d68060d613ed3464a473310bdfa00723f9528514
Murat Altun <mraltun@gmail.com>
2022-10-13 19:10:33 +0300

    Added login in authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,7 +6,25 @@ const asyncHandler = require("express-async-handler");
 // @desc Login
 // @route POST /auth
 // @access Public
-const login = asyncHandler(async (req, res) => {});
+const login = asyncHandler(async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({
+      message: "All fields are required",
+    });
+  }
+
+  const foundUser = await User.findOne({ username }).exec();
+
+  if (!foundUser || !foundUser.active) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const match = await bcrypt.compare(password, foundUser.password);
+
+  if (!match) return res.status(401).json({ message: "Unauthorized" });
+});
 
 // @desc Refresh
 // @route GET /auth/refresh
