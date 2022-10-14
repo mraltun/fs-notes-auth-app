958e17813e013c7c4d0f6ce4c80f6819358d231c
Murat Altun <mraltun@gmail.com>
2022-10-14 15:26:48 +0300

    Added authApiSlice

---
+++ client/src/features/auth/authApiSlice.js
@@ -0,0 +1,41 @@
+import { apiSlice } from "../../app/api/apiSlice";
+import { logOut } from "./authSlice";
+
+export const authApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: "/auth",
+        method: "POST",
+        body: { ...credentials },
+      }),
+    }),
+    sendLogout: builder.mutation({
+      query: () => ({
+        url: "/auth/logout",
+        method: "POST",
+      }),
+      // A function that is called when the individual mutation is started. The function is called with a lifecycle api object containing properties such as queryFulfilled, allowing code to be run when a query is started, when it succeeds, and when it fails (i.e. throughout the lifecycle of an individual query/mutation call).
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        try {
+          // Promise that will resolve with the (transformed) query result. If the query fails, this promise will reject with the error. This allows you to await for the query to finish.
+          const data = await queryFulfilled;
+          console.log(data);
+          dispatch(logOut());
+          dispatch(apiSlice.util.resetApiState());
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
+    refresh: builder.mutation({
+      query: () => ({
+        url: "/auth/refresh",
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation, useSendLogoutMutation, useRefreshMutation } =
+  authApiSlice;
