6031334981d465325bf631c5b376190ceae863a3
Murat Altun <mraltun@gmail.com>
2022-10-15 16:37:16 +0300

    Added useAuth to DashFooter and Welcome to fix routes for roles

--- client/src/components/DashFooter.jsx
+++ client/src/components/DashFooter.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
 const DashFooter = () => {
+  const { username, status } = useAuth();
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
@@ -25,8 +27,8 @@ const DashFooter = () => {
   const content = (
     <footer className='dash-footer'>
       {goHomeButton}
-      <p>Current User:</p>
-      <p>Status:</p>
+      <p>Current User: {username}</p>
+      <p>Status: {status}</p>
     </footer>
   );
 
